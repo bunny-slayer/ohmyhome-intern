38705043be675dbcf9031d6041e32b5c494df4e1
Vorapon Kachapornkul <hakao97@gmail.com>
2023-10-16 20:48:51 +0700

    Implement Load More button

--- frontend_depreciated/src/components/Loadmore.tsx
+++ frontend_depreciated/src/components/Loadmore.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const LoadMoreButton = () => {
+type LoadMoreButtonProps = {
+  loadMore: () => void; // A function to load more data
+};
+
+const LoadMoreButton = ({ loadMore }: LoadMoreButtonProps) => {
     return (
-        <button
-            className="justify-center items-center bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange- focus:outline-none "
+      <button
+      className="justify-center items-center bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange- focus:outline-none "
+        onClick={loadMore}
         >
             Load More
         </button>
--- frontend_depreciated/src/pages/home.tsx
+++ frontend_depreciated/src/pages/home.tsx
@@ -1,40 +1,46 @@
 "use client"
 
-import { JSX, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Cards from '@/components/Cards';
 import Header from '@/components/Header';
 import Homecard from '@/components/Homecard';
 import LoadMoreButton from '@/components/Loadmore';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Homepage = () => {
   const [data, setData] = useState([]);
+  const [visibleItems, setVisibleItems] = useState(10); // Initial number of items to display
+  const [totalProperties, setTotalProperties] = useState(0);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const limit = 10;
-      const offset = 0;
-
-      // Fetch data from your API
-      const response = await fetch(
-        `https://m9ojazlunf.execute-api.ap-southeast-1.amazonaws.com/test?limit=${limit}&offset=${offset}`
-      );
-
-      const result = await response.json();
-
-      if (result && result.list) {
-        setData(result.list);
-      }
-    };
-
-    fetchData();
+    fetchData(0, visibleItems);
   }, []);
+
+  const fetchData = async (offset: number, limit: number) => {
+    const response = await fetch(
+      `https://m9ojazlunf.execute-api.ap-southeast-1.amazonaws.com/test?limit=${limit}&offset=${offset}`
+    );
+    const result = await response.json();
+
+    if (result && result.list) {
+      setData(result.list);
+      setTotalProperties(result.totalProperties);
+    }
+  };
+
+  const handleLoadMore = () => {
+    // Load 4 more items when the "Load More" button is clicked
+    const newVisibleItems = visibleItems + 4;
+    fetchData(visibleItems, newVisibleItems);
+    setVisibleItems(newVisibleItems);
+  };
 
   return (
     <div>
       <Header />
       <div className="p-8 lg:p-24 xl:p-32">
         <Cards>
-          {data.map((item, index) => (
+          {data.slice(0, visibleItems).map((item, index) => (
             <Homecard
               key={index}
               title={item.title}
@@ -44,10 +50,12 @@ const Homepage = () => {
             />
           ))}
         </Cards>
-        <LoadMoreButton />
+        {visibleItems < totalProperties && (
+          <LoadMoreButton loadMore={handleLoadMore} />
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Homepage;
