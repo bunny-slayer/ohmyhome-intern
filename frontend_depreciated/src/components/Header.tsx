9c5ec2fd87c62e5059bb1e0d4a74538d5946e59e
Vorapon Kachapornkul <hakao97@gmail.com>
2023-10-16 21:48:06 +0700

    Implement Infinite Scroll and Fixed Header

--- frontend_depreciated/src/components/Header.tsx
+++ frontend_depreciated/src/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header className="flex items-center justify-center bg-white shadow-md p-4">
+        <header className="flex items-center justify-center bg-white shadow-md p-4" style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 100 }}>
             <Image
                 src="/logo.png"
                 alt="Logo"
--- frontend_depreciated/src/pages/home.tsx
+++ frontend_depreciated/src/pages/home.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from 'react';
 import Cards from '@/components/Cards';
@@ -10,12 +10,24 @@ const Homepage = () => {
   const [data, setData] = useState([]);
   const [visibleItems, setVisibleItems] = useState(10); // Initial number of items to display
   const [totalProperties, setTotalProperties] = useState(0);
+  const [loading, setLoading] = useState(false); // For indicating loading state
 
   useEffect(() => {
     fetchData(0, visibleItems);
+    // Add a scroll event listener for infinite scrolling on mobile
+    if (window.innerWidth <= 768) {
+      window.addEventListener('scroll', handleScroll);
+    }
+    return () => {
+      // Remove the scroll event listener when the component unmounts
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []); // Only run this effect on initial render
 
   const fetchData = async (offset: number, limit: number) => {
+    if (loading) return; // Prevent making multiple requests while loading
+
+    setLoading(true); // Set loading state
     const response = await fetch(
       `https://m9ojazlunf.execute-api.ap-southeast-1.amazonaws.com/test?limit=${limit}&offset=${offset}`
     );
@@ -25,13 +37,29 @@ const Homepage = () => {
       setData((prevData) => [...prevData, ...result.list]);
       setTotalProperties(result.totalProperties);
     }
+
+    setLoading(false); // Reset loading state
   };
 
   const handleLoadMore = () => {
+    if (loading) return; // Prevent clicking multiple times while loading
+
     // Load 4 more items when the "Load More" button is clicked
     const newVisibleItems = visibleItems + 4;
     fetchData(visibleItems, newVisibleItems);
     setVisibleItems(newVisibleItems);
+  };
+
+  const handleScroll = () => {
+    if (loading) return; // Prevent making multiple requests while loading
+
+    // Implement infinite scrolling for mobile
+    if (
+      window.innerHeight + window.scrollY >= document.body.scrollHeight - 200
+    ) {
+      const newVisibleItems = visibleItems + 4;
+      fetchData(visibleItems, newVisibleItems);
+    }
   };
 
   return (
@@ -49,9 +77,10 @@ const Homepage = () => {
             />
           ))}
         </Cards>
-        {visibleItems < totalProperties && (
+        {visibleItems < totalProperties && window.innerWidth > 768 && (
           <LoadMoreButton loadMore={handleLoadMore} />
         )}
+        {loading && <p>Loading...</p>} {/* Display loading indicator */}
       </div>
     </div>
   );
